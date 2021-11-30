aa93920d256a16432dd560c4a114ebcba5e4e6f6
VojtaSara <vojta.sara@gmail.com>
2021-11-30 13:01:44 +0100

    Procedural Mesh Voxels
    First main implementation of the Voxels inspired from a tutorial on youtube:https://www.youtube.com/watch?v=bFKgiPomYpM&ab_channel=BoardToBitsGames

--- VoxelSculpt/Assets/VoxelData.cs
+++ VoxelSculpt/Assets/VoxelData.cs
@@ -27,4 +27,52 @@ public class VoxelData
     {
         return data[x, y, z];
     }
+
+    public int GetNeighbour(int x, int y, int z, Direction dir)
+    {
+        DataCoordinate offsetToCheck = offsets[(int)dir];
+        DataCoordinate neighborCoord = new DataCoordinate(x + offsetToCheck.x, y + offsetToCheck.y, z + offsetToCheck.z);
+        if (neighborCoord.x < 0 || neighborCoord.x >= Width || neighborCoord.y < 0 || neighborCoord.y >= Depth || neighborCoord.z < 0 || neighborCoord.z >= Height)
+        {
+            return 0;
+        }
+        else
+        {
+            return GetCell(neighborCoord.x, neighborCoord.y, neighborCoord.z);
+        }
+    }
+
+    struct DataCoordinate
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public DataCoordinate(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    DataCoordinate[] offsets =
+    {
+        new DataCoordinate(0, 0, 1),
+        new DataCoordinate(1, 0, 0),
+        new DataCoordinate(0, 0, -1),
+        new DataCoordinate(-1, 0, 0),
+        new DataCoordinate(0, 1, 0),
+        new DataCoordinate(0, -1, 0)
+    };
+}
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+    Up,
+    Down
 }
